71e10d79bab13b3fc0d599f0792dd5e4dc7787dc
VauReo <kushnir.ea@phystech.edu>
2022-10-04 03:44:08 +0300

    Сделал для людей поле слипскеджуал, настроил авг_слеепэжд и гетторы с сетторами для них

--- Human_being.cpp
+++ Human_being.cpp
@@ -8,12 +8,27 @@
 using namespace std;
 
 
-void Human_being::set_weight(int in_weight) {
-    this->weight = in_weight;
+void Human_being::moan() {
+    cout << "Yamete" << endl;
 }
+
+void Human_being::sleep(short a) {
+    for (int i = 0; i < 6; ++i) {
+        this->sleep_schedule[i] = this->sleep_schedule[i+1];
+    }
+    this->sleep_schedule[6] = a;
+
+    this->set_avg_sleepage();
+}
+
+
 
 void Human_being::set_height(int in_height) {
     this->height = in_height;
+}
+
+void Human_being::set_weight(int in_weight) {
+    this->weight = in_weight;
 }
 
 void Human_being::set_age(int in_age) {
@@ -24,13 +39,29 @@ void Human_being::set_name(string in_name) {
     this->name = in_name;
 }
 
+/*
 void Human_being::set_avg_sleep(int in_set_avg_sleep) {
     this->avg_sleepage = in_set_avg_sleep;
 }
+*/
 
-void Human_being::moan() {
-    cout << "Yamete" << endl;
+void Human_being::set_sleep_schedule(short a[7]) {
+    for (int i = 0; i < 7; ++i) {
+        this->sleep_schedule[i] = a[i];
+    }
+
+    this->set_avg_sleepage();
 }
+
+void Human_being::set_avg_sleepage() {
+    short tmp = 0;
+    for (int i = 0; i < 7; ++i) {
+        tmp += this->sleep_schedule[i];
+    }
+    this->avg_sleepage = int(tmp/6);
+}
+
+
 
 string Human_being::get_name() {
     return this->name;
@@ -40,15 +71,33 @@ int Human_being::get_age() {
     return this->age;
 }
 
+int Human_being::get_height() {
+    return this->height;
+}
+
+int Human_being::get_weight() {
+    return this->weight;
+}
+
+int Human_being::get_avg_sleepage() {
+    return this->avg_sleepage;
+}
+
+short * Human_being::get_sleep_schedule() {
+    return this->sleep_schedule;
+}
+
+
 /*
 int main() {
     cout << "sdasd" << endl;
-    Human_being Ger(5, "gerold");
+    Human_being Ger("gerold", 5);
     Ger.moan();
     Ger.set_age(6);
     Ger.set_name("Germany");
     cout << Ger.get_age() << ' ' << Ger.get_name() << endl;
-
+    //Ger.sleep(5);
+    cout << Ger.get_sleep_schedule()[0] << endl;
 
 }
 */
--- Human_being.h
+++ Human_being.h
@@ -12,24 +12,28 @@ using namespace std;
 class Human_being{
     int weight = 60;
     int height = 160;
-    int avg_sleepage = 0;
+    int avg_sleepage; //average amount of minutes you sleep
     int age;
+    short sleep_schedule[7] = {360, 360, 360, 360, 360, 360, 360};
+                      //arr with durations of ur sleep for every day on last week
 
 public:
     string name;
+
     Human_being(string name, int age): name(name), age(age) {};
 
-    void set_weight(int a);
-    void set_height(int a);
-    void set_avg_sleep(int a);
-    void set_age(int a);
-    void set_name(string a);
-
-    void sleep(int a);
+    void sleep(short a);
     void moan();
 
+    void set_height(int a), set_weight(int a), set_avg_sleepage(), set_age(int a);
+    void set_sleep_schedule(short a[7]);
+    void set_name(string a);
+
+
+    int get_height(), get_weight(), get_avg_sleepage(), get_age();
+    short * get_sleep_schedule();
     string get_name();
-    int get_age();
+
 
 };
 
